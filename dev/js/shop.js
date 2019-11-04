fcfaf57cf88c9e06a5c01d801f400bcdffdc604f
Chienyuwen0210 <g109642021@gmail.com>
2019-11-05 07:48:49 +0800

    鈺玟11/5商品內頁增加表單與燈箱

--- dev/js/shop.js
+++ dev/js/shop.js
@@ -5,18 +5,28 @@ window.addEventListener('load',function(){
     document.getElementsByClassName("productBtn")[i].onclick = shopShowLightBox;
     }
     document.getElementById("Cancelbtn").onclick = cancelLogin;
-    // document.getElementById("productBox")=cancelProductBox;
     document.getElementById("addToBackpack").onclick = addToBackpack01;
+    document.getElementById("eventButton").onclick = trashBack;
+    document.getElementById("trashLightBoxBtn").onclick = showGashaoin;
     });
-    
     function shopShowLightBox(){
         document.getElementById("shopLightBox").style.display="";
         document.getElementById("boxForDisappear").style.opacity="0";
     }  
     function cancelLogin(){
         document.getElementById("shopLightBox").style.display = "none";
-        document.getElementById("boxForDisappear").style.opacity="1 ";
+        document.getElementById("boxForDisappear").style.opacity="1";
     }
     function addToBackpack01(){
         document.getElementById("shopLightBox").style.display = "none";
+    }
+    function trashBack(){
+        document.getElementById("event").style.opacity = "0";
+        document.getElementById("trashLightBox").style.opacity= "1";
+        document.getElementById("gashaponBtn").style.opacity="0";
+    }
+    function showGashaoin(){
+        document.getElementById("event").style.opacity = "1";
+        document.getElementById("trashLightBox").style.opacity ="0"
+        document.getElementById("gashaponBtn").style.opacity="1";
     }
