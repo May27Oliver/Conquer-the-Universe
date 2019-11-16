b0f95b5907629933647711dce8fd7bb8747ca36e
May27Oliver <oliver310338@hotmail.com>
2019-11-16 15:31:52 +0800

    公民投票增加倒數

--- dev/js/VoteClick.js
+++ dev/js/VoteClick.js
@@ -36,6 +36,7 @@ window.addEventListener("load",function(){
     let voteVotingNo = $class(".voteVotingNo");
     let voteCard = $classes(".voteCard");
     let voter = $class(".voter");
+    let countDown= $classes(".deadline");
 
 
     /*__________ 圓餅圖製作 __________*/
@@ -84,15 +85,11 @@ window.addEventListener("load",function(){
             var prosNum=voteArr[i].prosNum;
             var consNum=voteArr[i].consNum;
             pieChart[i].data.datasets[0].data=[prosNum,consNum];
-            
-        //將隨機資料載入至圖表中
-        // pieChart[i].data.datasets[0].data=[prosNum,consNum];
-        //物件.prosNum  物件.consNum
         //更新
         pieChart[i].update();
         }
     }
-    setInterval(changeData,500);
+    setInterval(changeData,1000);//
 
     function insertAfter(newEl, targetEl){//將element元素插到node後面
       var parentEl = targetEl.parentNode;
@@ -116,7 +113,7 @@ window.addEventListener("load",function(){
     }
     var voteArr=[];
     
-    voteArr=[{prosNum:5,consNum:8},{prosNum:0,consNum:0},{prosNum:0,consNum:0},{prosNum:0,consNum:0},{prosNum:0,consNum:0}];
+    voteArr=[{prosNum:5,consNum:8},{prosNum:2,consNum:1},{prosNum:1,consNum:2},{prosNum:3,consNum:2},{prosNum:2,consNum:3}];
     
     function getIndex(child){//取button的index值
         var parent = child.parentNode.parentNode.parentNode.parentNode.parentNode;
@@ -129,13 +126,48 @@ window.addEventListener("load",function(){
             }
         }
         return i;
-    };  
+    }; 
+
+    // 倒數計時截止生產器
+    var countDownDate = new Date().getTime()+ 86400000;//改成24hour
+    var rightnow = new Date().getTime();
+    console.log(countDownDate);
+    console.log(rightnow);
+    console.log(countDownDate-rightnow);
+
+    // Update the count down every 1 second
+    var x = setInterval(function() { 
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        for(var i=0;i<countDown.length;i++){    
+        // Display the result in the element with id="demo"
+            countDown[i].innerHTML = "投票時間剩："+hours + "時"
+            + minutes + "分 " + seconds + "秒";
+
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                countDown[i].innerHTML = "EXPIRED";
+            }
+        }
+    }, 1000);
+
+
 
     // 新增公民投票議題出現，事件聆聽功能
     voteAddingSubmit.addEventListener('click',function(){
         var name=voter.value;//這邊要記錄發起人的會員Id
         var title=voteTitle.value;
-        var deadline=2019-11-14;
+        var deadline=x;
         var prosNum=0;
         var consNum=0;
         voteArr.unshift(new voteObj(name,title,deadline,prosNum,consNum));
@@ -164,6 +196,7 @@ window.addEventListener("load",function(){
         wrapeDiv4.className = "voteText";
         wrapeDiv5.className = "voteSelectGroup";
         pieCanvas.className = "votePie";
+        deadline.className="deadline";
         reportBtn.className = "report";
         yesBtn.className = "voteYes";
         noBtn.className = "voteNo";
@@ -182,13 +215,34 @@ window.addEventListener("load",function(){
         insertAfter(voteIssue,issuePerson);
         voteIssue.innerText=voteTitle.value;
         insertAfter(deadline,voteIssue);
-        deadline.innerText="2019/11/05號截止";
+        // deadline.innerText="2019/11/05號截止";
         insertAfter(wrapeDiv5,deadline);
         wrapeDiv5.appendChild(yesBtn);
         yesBtn.innerText=voteSelectorA.value;
         insertAfter(noBtn,yesBtn);
         noBtn.innerText=voteSelectorB.value;
         
+        deadline.onload=setInterval(function() { 
+            // Get today's date and time
+            var now = new Date().getTime();
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+    
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
+            // Display the result in the element with id="demo"
+                deadline.innerHTML = "投票時間剩："+hours + "時"
+                + minutes + "分" + seconds + "秒";
+                // If the count down is finished, write some text
+                if (distance < 0) {
+                    clearInterval(x);
+                    deadline.innerHTML = "EXPIRED";
+                }
+        }, 1000);//新增的投票倒數器
+
         yesBtn.onclick=function(){
             var yesIndex=getIndex(this)-1;
             voteDoingNotice.innerHTML="確認後將無法更改，<br>您要選擇此投票項目嗎？";
@@ -226,6 +280,18 @@ window.addEventListener("load",function(){
                 console.log(voteArr[noIndex].consNum);
             };
         }//增加yesBtn function和noBtn function
+
+        reportBtn.onclick = function(){
+            voteDoingNotice.innerText="檢舉原因：";
+            voteAddingSubmit.style.display="none";
+            // voteVotingSubmit.style.display="none";
+            voteReportSubmit.style.display="inline-block";
+            voteReportMessage.style.display="block";
+            voteVotingYes.style.display="none";
+            voteVotingNo.style.display="none";
+            voteAlertGroup.style.display="block";
+            voteAlertDoing.style.display="block";
+        }
 
         pieProduce();
         // voting();        
