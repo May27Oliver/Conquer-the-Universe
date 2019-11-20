366c206a670fda6551dd8c08890f5c9b0d063f40
dayzero88 <dayo0401@gmail.com>
2019-11-20 14:41:22 +0800

    BIG123

---
+++ dev/js/news.js
@@ -0,0 +1,9 @@
+function $id(id){
+    return document.getElementById(id);
+}
+function $class(className){
+    return document.querySelector(className);
+}
+function $classes(classNames){
+    return document.querySelectorAll(classNames);
+}
