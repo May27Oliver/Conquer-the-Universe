3851615345063e2b6bf580d314f797e3b3d21545
hvidchu <cizyhane@gmail.com>
2019-11-21 00:22:24 +0800

    阿白可以結算ㄌ!!!!

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -1,11 +1,6 @@
 window.addEventListener('load',function(){
 
-   let getStart=document.getElementById("winPoint_1");
-   if(getStart){
-      new CountUp("winPoint_1",0,66666).start();
-      new CountUp("winPoint_2",0,66666).start();
-      new CountUp("winPoint_3",0,66666).start();
-   }
+   
    let homeCitizenCount = document.getElementById("popularity");
    if(homeCitizenCount){
       new CountUp("popularity",0,66666).start();
