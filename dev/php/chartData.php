a79f9c4e6b1db62e3c1d9a533df4cd4ff6197115
dengmeidada <k333441@gmail.com>
2019-11-20 19:31:47 +0800

    公民的家PHP及增加星球圖

---
+++ dev/php/chartData.php
@@ -0,0 +1,19 @@
+<?php
+//個人編輯會員資料撈取
+$errMsg="";
+try{
+  require_once("meiDa.php");
+  $sql2 = "select wisdom,charisma,finance from `member` order by memNo limit 1" ;
+  $chartData=$pdo->query($sql2);
+  $arr=[];
+  for($i=0;$i<6;$i++){
+    $chartDataRow=$chartData->fetch(PDO::FETCH_ASSOC);
+    $arr[$i]=$chartDataRow;
+}
+echo json_encode($arr);
+}catch(PDOException $e){
+  $errMsg.="錯誤原因：".$e->getMessage()."<br>";
+  $errMsg.="錯誤行號：".$e->getLine()."<br>";
+}
+echo $errMsg;
+?>
