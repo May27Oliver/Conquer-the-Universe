b0213c942fe883a136c822e8191a6144b55ab225
May27Oliver <oliver310338@hotmail.com>
2019-11-21 13:41:51 +0800

    會員登出功能

--- dev/js/login.js
+++ dev/js/login.js
@@ -22,6 +22,7 @@
     let navTop=$class(".navTop");
     let wealth = $id("wealth");
     let coin = $id("coin");
+    let loginMsg=$id("loginMsg");
     //__________ 燈箱開關 __________//
     //燈箱打開
     function loginRegister(){
@@ -35,7 +36,6 @@
         loginWrap.style.display="none";
     }
     //登入及驗證
-    function login(){}
     function registerBox(){
         loginPage.style.display="none";
         registerPage.style.display="";
@@ -67,21 +67,24 @@
                     wealth.style.backgroundImage.url="../img/GEM.png";
                     wealth.style.backgroundRepeat="none";
                     coin.innerText="800";
+                    wealth.style.width=23+"px";
+                    wealth.style.height=23+"px";
                     // navTop.style.top=40+"px";
                     // banner.style.top=70+"px";
                     // topBg.style.height=170+"px";
                     let member = JSON.parse(xhr.responseText);
                     if( member.error){
-
+                        loginMsg.innerText=member.error;
                     }else{
                         console.log("=====",member.memId);
                         console.log("=====",member.memName);
                         console.log("=====",member.email);                      
                     }
                     // memName.style.top=35;
-                    memberName.innerText="您好!&nbsp&nbsp"+member.memName;
+                    memberName.innerText="您好!"+member.memName;
                     loginBtn.innerText="登出";
-                // }
+                    loginBtn.onclick=null;
+                    loginBtn.onclick=logOut;
             }else{
                 document.getElementById("loginMsg").innerText=xhr.status;
             }   
@@ -94,6 +97,26 @@
         xhr.send(query_string);
         console.log(query_string);
     } 
+    /*--------------登出-------------------*/
+    function logOut(){
+        let xhr = new XMLHttpRequest();
+        xhr.onload=function(){
+            if(xhr.status==200){
+                loginBtn.innerHTML = "登入/註冊";
+                memberName.innerHTML ="";
+                wealth.style.width=0;
+                wealth.style.height=0;
+                coin.innerText="";
+                loginBtn.style.width=100+"px";
+                loginBtn.onclick=null;
+                loginBtn.onclick=registerBox;
+                //這裡的事件聆聽似乎沒有寫進去
+            }
+        }
+        xhr.open("get", "php/logOut.php", true);
+        xhr.send(null);
+    }
+
     /*-----------註冊帳號驗證--------------*/
     function checkId(){
 
@@ -105,7 +128,7 @@ window.addEventListener("load",function(){
     xhr.onload=function(){
       let member = JSON.parse(xhr.responseText);
       if(member.memName){
-        memberName.innerHTML = "您好!&nbsp&nbsp"+member.memName;
+        memberName.innerHTML = "您好!"+member.memName;
         // navTop.style.top=40+"px";
         // banner.style.top=70+"px";
         // topBg.style.height=170+"px";
@@ -114,8 +137,9 @@ window.addEventListener("load",function(){
         wealth.style.backgroundRepeat="none";
         coin.innerText="800";
         loginBtn.style.width=50+"px"
-        $id("loginBtn").innerHTML = "登出";
-        // $id("loginBtn").onclick=logOut();
+        loginBtn.innerHTML = "登出";
+        loginBtn.onclick=null;
+        loginBtn.onclick=logOut;
       }
     }
     xhr.open("get", "php/getLoginInfo.php", true);
--- dev/php/logOut.php
+++ dev/php/logOut.php
@@ -4,4 +4,5 @@ unset($_SESSION["memNo"]);
 unset($_SESSION["memId"]);
 unset($_SESSION["memName"]);
 unset($_SESSION["email"]);
+session_destroy();
 ?>
