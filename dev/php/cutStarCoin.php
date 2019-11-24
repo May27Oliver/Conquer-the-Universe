7d677ac7192170c52f457680b2183bc147a8c571
May27Oliver <oliver310338@hotmail.com>
2019-11-25 00:17:49 +0800

    商城轉蛋連資料庫成功

---
+++ dev/php/cutStarCoin.php
@@ -0,0 +1,42 @@
+<?php 
+require_once('connectPDO.php');
+session_start();
+$event=$_GET["event"];
+$popNum=100;
+try{
+    $memberNo=$_SESSION["memNo"];
+    $sql="select starCoin from `member` where memNo={$memberNo}";
+    $money = $pdo->prepare($sql);
+    $money->execute();
+    $moneyCount = $money->fetch(PDO::FETCH_ASSOC);
+    if($moneyCount["starCoin"]>100){
+        $moneyCut=100;
+        if($event==0 || $event==2){
+            //支持度扣一百
+            $sql="UPDATE `member`
+            SET `starCoin` = starCoin-{$moneyCut},`popularity` = popularity-{$popNum}
+            WHERE `member`.`memNo` = {$memberNo}";
+        }elseif($event==1){
+            //支持度加一百
+            $sql="UPDATE `member`
+            SET `starCoin` = starCoin-{$moneyCut},`popularity` = popularity+{$popNum}
+            WHERE `member`.`memNo` = {$memberNo}";
+        }
+        $pdo->exec($sql);
+
+        $sql="select starCoin from `member` where memNo={$memberNo}";
+        $afterCut = $pdo->prepare($sql);
+        $afterCut->execute();
+        $number = $afterCut->fetch(PDO::FETCH_ASSOC);
+        $_SESSION["starCoin"] = $number["starCoin"]; //新增
+
+        $arr["starCoin"] = $number["starCoin"]; //新增
+
+        echo $number["starCoin"];
+    }else{
+        echo "0";
+    }
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+?>
