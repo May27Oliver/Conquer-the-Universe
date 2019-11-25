ff3f96f0c72761ba4521ae40601a5acc77a45cdb
lllulllu <liaoaura@gmail.com>
2019-11-25 18:32:59 +0800

    陽光普照拿金馬

--- dev/php/vote/gainNoTicket.php
+++ dev/php/vote/gainNoTicket.php
@@ -11,21 +11,27 @@ try{
         "INSERT INTO `votrecord` (votNo, memNo) 
         VALUES ({$votNo}, {$memNo})";
     $pdo->exec($sql_record);
-    echo "新增成功！";
     
     $sql_voteB = 
         "UPDATE `vote` 
         SET `votBCount` = `votBCount`+ 1 
         WHERE `vote`.`votNo` = {$votNo}";
     $pdo->exec($sql_voteB);
-    echo "更新成功！";
 
     $sql_addCoin = 
         "UPDATE `member` 
         SET `starCoin` = `starCoin`+ 10 
         WHERE `member`.`memNo` = {$memNo}";
     $pdo->exec($sql_addCoin);
-    echo "更新成功！";
+
+    //扣完宇宙幣後要把新的宇宙幣數值fetch回來，並存進session裡面
+    $sql_afterCut="select starCoin from `member` where memNo={$memNo}";
+    $afterCut = $pdo->prepare($sql_afterCut);
+    $afterCut->execute();
+    $number = $afterCut->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["starCoin"] = $number["starCoin"];
+
+    echo $number["starCoin"];
 
 }catch(PDOException $e){
     $errMsg .= "錯誤訊息: {$e->getMessage()}<br>";
